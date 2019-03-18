bc87dd614344bad67ba616da14a6418e29791513
mirco <mircobaalmans@live.nl>
2019-03-18 11:21:44 +0100

    different message function with color option

--- BFW/debug.cpp
+++ BFW/debug.cpp
@@ -8,3 +8,34 @@ Debug::~Debug(){
 
 
 }
+
+void Debug::message(std::string msg, termColor color) {
+	switch (color) {
+		case 0	: //grey
+			std::cout << termcolor::reset << termcolor::grey << msg << std::endl;
+			break;
+		case 1: //red
+			std::cout << termcolor::reset << termcolor::red << msg << std::endl;
+			break;
+		case 2: //green
+			std::cout << termcolor::reset << termcolor::green << msg << std::endl;
+			break;
+		case 3: //yellow
+			std::cout << termcolor::reset << termcolor::yellow<< msg << std::endl;
+			break;
+		case 4: //blue
+			std::cout << termcolor::reset << termcolor::blue << msg << std::endl;
+			break;
+		case 5: //magenta
+			std::cout << termcolor::reset << termcolor::magenta << msg << std::endl;
+			break;
+		case 6: //cyan
+			std::cout << termcolor::reset << termcolor::cyan << msg << std::endl;
+			break;
+		case 7: //white
+			std::cout << termcolor::reset << termcolor::white << msg << std::endl;
+			break;
+
+	}
+
+}
--- BFW/debug.h
+++ BFW/debug.h
@@ -8,8 +8,23 @@
 
 class Debug{
 public:
+	static enum termColor {
+		grey,
+		red,
+		green,
+		yellow,
+		blue,
+		magenta,
+		cyan,
+		white
+	};
+
 	//output a message to the console
 	static void message(std::string msg) { std::cout <<  std::endl << msg << std::endl; }
+	static void message(std::string msg, termColor color);
+
+	
+
 private:
 	Debug();
 	virtual ~Debug();
