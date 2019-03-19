3a0962f180a210ae81333c413054946916bc14a6
mirco <mircobaalmans@live.nl>
2019-03-19 12:30:33 +0100

    removed double use of termcolor::reset

--- BFW/debug.cpp
+++ BFW/debug.cpp
@@ -16,31 +16,31 @@ void Debug::message(std::string msg, termColor color) {
 			std::cout << termcolor::reset;
 			break;
 		case 1: //red
-			std::cout << termcolor::reset << termcolor::red << msg << std::endl;
+			std::cout << termcolor::red << msg << std::endl;
 			std::cout << termcolor::reset;
 			break;
 		case 2: //green
-			std::cout << termcolor::reset << termcolor::green << msg << std::endl;
+			std::cout << termcolor::green << msg << std::endl;
 			std::cout << termcolor::reset;
 			break;
 		case 3: //yellow
-			std::cout << termcolor::reset << termcolor::yellow<< msg << std::endl;
+			std::cout << termcolor::yellow<< msg << std::endl;
 			std::cout << termcolor::reset;
 			break;
 		case 4: //blue
-			std::cout << termcolor::reset << termcolor::blue << msg << std::endl;
+			std::cout << termcolor::blue << msg << std::endl;
 			std::cout << termcolor::reset;
 			break;
 		case 5: //magenta
-			std::cout << termcolor::reset << termcolor::magenta << msg << std::endl;
+			std::cout << termcolor::magenta << msg << std::endl;
 			std::cout << termcolor::reset;
 			break;
 		case 6: //cyan
-			std::cout << termcolor::reset << termcolor::cyan << msg << std::endl;
+			std::cout << termcolor::cyan << msg << std::endl;
 			std::cout << termcolor::reset;
 			break;
 		case 7: //white
-			std::cout << termcolor::reset << termcolor::white << msg << std::endl;
+			std::cout << termcolor::white << msg << std::endl;
 			std::cout << termcolor::reset;
 			break;
 
